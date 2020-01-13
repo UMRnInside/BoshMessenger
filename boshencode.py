3bb105e22c258d9d943dfbc4209b2d392044a46e
UMRnInside <newpage2@foxmail.com>
2020-01-13 23:01:31 +0800

    Change 3-byte units to 2-byte ones

--- boshencode.py
+++ boshencode.py
@@ -1,28 +1,28 @@
 import boshdata
 import struct
 
-BCAP = 12
-BMASK = (2 ** BCAP) - 1
+BFCAP = 12
+BFMASK = (2 ** BFCAP) - 1
 
-def byte3_split(ibytes):
+def byte2_split(ibytes):
     "little-endian! returns a tuple like (11, 4095)"
-    ibytes += b"\0\0\0"
-    # Packing unsigned int, should be 4-byte 
-    num = struct.unpack(b"<I", ibytes[0:3] + b"\0")[0]
-    ret1 = num & BMASK
-    ret2 = (num >> BCAP) & BMASK
+    ibytes += b"\0\0"
+    # Packing unsigned short int, should be 2-byte 
+    num = struct.unpack(b"<H", ibytes[0:2])[0]
+    ret1 = num & 0x0F # 4-bit
+    ret2 = (num >> 4) 
     return (ret1, ret2)
 
 def gen_length_indicator(length):
     "The length indicator is in little-endian."
-    if length >= 2**(BCAP * 2):
+    if length >= 2**(16):
         raise ValueError("Too long! Ensure your data is shorter than " + str(2**(BCAP*2)))
-    sps = byte3_split(struct.pack("<I", length))
-    return tuple(map(lambda i: boshdata.fortunes[i], sps) )
+    sps = byte2_split(struct.pack("<I", length))
+    return boshdata.bosh[sps[0]] + boshdata.fortunes[sps[1]]
 
 def bytes_encode(bdata):
     for curbyte in iter(bdata):
         pass
 
 if __name__ == "__main__":
-    print(gen_length_indicator(103))
+    print(gen_length_indicator(42))
