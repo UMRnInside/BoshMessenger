100c905211cbbc793b6ddb00081f68603391fd6f
UMRnInside <newpage2@foxmail.com>
2020-01-13 12:34:22 +0800

    Add boshdata

---
+++ boshdata.py
@@ -0,0 +1,28 @@
+import readjson
+
+FORTUNE_CAP = 4096 # 2 ** 12
+
+datafile = "data.json"
+data = readjson.readJSON(datafile)
+
+fortune_skel = data["famous"]
+before = data["before"]
+after = data["after"]
+
+bosh = data["bosh"]
+fortunes = []
+
+# Bake fortunes
+for af in after:
+    for bef in before:
+        for forts in fortune_skel:
+            # Fortune skeleton goes like: "Mark Twain a, blah blah blah, b"
+            baked = forts.replace("a", bef).replace("b", af)
+            fortunes.append(baked)
+
+# Avoid changes
+fortunes = tuple(fortunes[:FORTUNE_CAP])
+
+if __name__ == "__main__":
+    # Test
+    print("\n".join(fortunes))
